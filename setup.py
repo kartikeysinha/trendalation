b0cce2bb6fbb51719d04c8bad2f7614390c1262d
Raghav Saboo <raghs2000@gmail.com>
2023-07-06 20:51:55 -0400

    Add setup files for building package

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trendalation',
+    version='0.0.1',
+    license='MIT',
+    author="Raghav Saboo, Kartikey Sinha",
+    author_email='raghs2000@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/kartikeysinha/trendalation',
+    keywords='anomaly detection',
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'scipy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+  ],
+)
