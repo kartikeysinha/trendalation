9b0088de5ad31608e362e38006aa5b6cd2dbce99
Raghav Saboo <raghs2000@gmail.com>
2023-07-08 12:48:33 -0400

    Code rafactoring & restructuring

---
+++ init.py
@@ -0,0 +1,2 @@
+from trendalation.classifiers import ProcClassifier
+from trendalation.metrics import proc_error, normalize_trace
--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='trendalation',
+    packages=['trendalation'],
     version='0.0.1',
     license='MIT',
     author="Raghav Saboo, Kartikey Sinha",
     author_email='raghs2000@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    # packages=find_packages('src'),
+    # package_dir={'': 'src'},
     url='https://github.com/kartikeysinha/trendalation',
-    keywords='anomaly detection',
+    keywords=['anomaly detection', 'procrustes', 'time-series', 'trends'],
     install_requires=[
         'numpy',
         'scikit-learn',
