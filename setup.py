848a076e0151cb2052bb6e87ff1c85c5716c80ff
Raghav Saboo <raghs2000@gmail.com>
2023-08-12 16:36:50 -0400

    Update project configuration to use README.md as project description

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+curr_dir = Path(__file__).parent
+long_description = (curr_dir / "README.md").read_text()
+
 setup(
     name='trendalation',
     package_dir={"": "src"},
@@ -10,11 +14,12 @@ setup(
     author=['Raghav Saboo', 'Kartikey Sinha'],
     author_email='raghs2000@gmail.com',
     url='https://github.com/kartikeysinha/trendalation',
-    keywords=['anomaly detection', 'procrustes', 'time-series', 'trends'],
+    keywords=['anomaly detection', 'time-series', 'procrustes', 'trends', 'spatial analysis'],
     install_requires=[
         'numpy',
         'scikit-learn',
-        'scipy'
+        'scipy',
+        'pytest'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -22,5 +27,9 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
-  ],
+    ],
+    
+    # Inherit description from README.md
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
